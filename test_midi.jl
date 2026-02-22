15dfb6cd3259dd8cbd68bf0d404e4097d589edb3
ardy-monaghan <ross.monaghan99@gmail.com>
2026-02-22 12:22:49 +0000

    Initial tracking now working

--- test_midi.jl
+++ test_midi.jl
@@ -1,8 +1,9 @@
 using BlobTracking, Images, VideoIO
 
-## Get the tracks
 
-path = "./bird_data/feb26/raw_swipe.MOV"
+# Get the tracks
+
+path = "./bird_data/feb26/tt.mp4"
 io   = VideoIO.open(path)
 vid  = VideoIO.openvideo(io)
 img  = first(vid)
@@ -21,10 +22,10 @@ bg = background(medbg)
 Gray.(bg)
 
 mask = ones(size(bg))
-mask[:,1100:end] .= 0
-mask[1:200,:] .= 0
+mask[1:270,:] .= 0
 
 Gray.(mask)
+
 
 function preprocessor(storage, img)
     storage .= Float32.(img)
@@ -32,11 +33,7 @@ function preprocessor(storage, img)
     storage .= Float32.(abs.(storage .- background(medbg)) .> 0.25) # You can save some computation by not calculating a new background image every sample
 end
 
-test_img = copy(img)
-preprocessor(test_img, img)
-test_img
-
-
+# Get coordinates
 bt = BlobTracker(5:10, #sizes 
                 2.0, # σw Dynamics noise std.
                 10.0,  # σe Measurement noise std. (pixels)
@@ -46,6 +43,10 @@ bt = BlobTracker(5:10, #sizes
                 correspondence = HungarianCorrespondence(p=1.0, dist_th=2), # dist_th is the number of sigmas away from a predicted location a measurement is accepted.
 )
 
+##
+# coords = get_coordinates(bt, vid)
+
+
 result = track_blobs(bt, vid,
                          display = nothing, # use nothing to omit displaying.
                          recorder = Recorder()) # records result to video on disk
@@ -53,11 +54,57 @@ result = track_blobs(bt, vid,
 
 ## Plot the tracks
 
-traces = trace(result, minlife=5) # Filter minimum lifetime of 5
+traces = trace(result, minlife=40) # Filter minimum lifetime of 5
 measurement_traces = tracem(result, minlife=5)
 drawimg = RGB.(img)
 draw!(drawimg, traces, c=RGB(0,0,0.5))
-draw!(drawimg, measurement_traces, c=RGB(0.5,0,0))
+# draw!(drawimg, measurement_traces, c=RGB(0.5,0,0))
+
+
+## Testing
+
+
+result = TrackingResult()
+buffer =  vid
+ws = BlobTracking.Workspace(img, length(bt.sizes))
+img, coord_or_img = img isa Tuple ? img : (img,img)
+measurement = Measurement(ws, bt, coord_or_img, result)
+
+# Update the results for each frame
+for (ind,img) in enumerate(buffer)
+
+    particle_shifted = Int64[]
+
+    # Update the blobs
+    println("Frame $ind")
+    img, coord_or_img = img isa Tuple ? img : (img,img)
+    measurement = update!(ws, bt, coord_or_img , result)
+
+    # Check which blobs are left of the line
+    # Loop over the blobs
+    for (bᵢ, blob) in enumerate(result.blobs)
+        if length(blob.trace) <= 1 
+            continue
+        end
+        if blob.tracem[end-1][2] < 580 && blob.tracem[end][2] >= 580
+            push!(particle_shifted, bᵢ)
+            println("Particle shifts at $(ind)")
+        end
+    end
+
+    # If position has switched play midi note
+end
+
+##
+
+
+for (ind,img) in enumerate(buffer)
+            println("Frame $ind")
+            img, coord_or_img = img isa Tuple ? img : (img,img)
+            measurement = update!(ws, bt, coord_or_img , result)
+            showblobs(RGB.(Gray.(img)),result,measurement, rad=6, recorder=recorder, display=display, ignoreempty=ignoreempty)
+        end
+
 
 ## Get the midi notes 
 
